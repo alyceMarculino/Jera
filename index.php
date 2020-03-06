4e18b753f34568cb3004395170e7a60a512876ca
alyceMarculino <61853841+alyceMarculino@users.noreply.github.com>
2020-03-05 20:07:27 -0400

    Create index.php
    Índice básico com dois bottons

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/index.css">
+		<title> </title>
+	</head>
+	
+	<body>
+		<div id="site">
+			<div id="conteudo">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<form class ="form-signin" method="POST">
+								<button class="btn btn-light btn-outline-dark"> <a href="novaConta.php"> Criar Nova Conta </a> </button>
+							</form>
+						</div>
+						<div class="col">
+							<form class ="form-signin" method="POST">
+								<button class="btn btn-light btn-outline-dark"> <a href="login.php"> Jรก sou Cadastrado </a> </button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	
+</html>
+
